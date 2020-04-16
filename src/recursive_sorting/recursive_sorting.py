c49eb3abc333bfa5c68af5bce2da73dc932974b5
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-04-15 17:16:34 -0700

    MVP Day 2

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,58 +2,53 @@
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    leftindex = 0
-    rightindex = 0
     # TO-DO
-    while(leftindex < len(arrA) and rightindex < len(arrB)):
-        if(arrB)[rightindex] > arrA[leftindex]:
-            merged_arr.append(arrA[leftindex])
-            leftindex += 1
+    # put back together here
+    # Sorting happens here
+
+    a = 0
+    b = 0 
+
+    for k in range(0, elements):
+        # What do we do in here?
+        # Compare a and b
+        # if a is out of range, push b and iterate
+        if a >= len(arrA): # we're done with a, push B
+            merged_arr[k] = arrB[b]
+            b += 1
+        # if b is out of range, push a and iterate
+        elif b >= len(arrB): # we're done with a, push B
+            merged_arr[k] = arrA[a]
+            a += 1
+        # if a is bigger, put it in an array and iterate both
+        elif arrA[a] < arrB[b]: # we're done with a, push B
+            merged_arr[k] = arrA[a]
+            a += 1
+        # if b is smaller put it in an array and iterate both
         else:
-            merged_arr.append(arrB)[rightindex]
-            rightindex += 1
+            merged_arr[k] = arrB[b]
+            b +=1
 
     return merged_arr
-print(merge)
+
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-    # (base case) If the array is empty or length 1, return
-    if len(arr)>1:
-        #spints the array into half
-        mid = len(arr)//2
-        lefthalf = arr[:mid]
-        righthalf = arr[:mid]
+    # Split here
+    # base condition
+    # if array size >= 1
+    if len(arr) > 1:
+    # find the middle of arr
+    # sort stuff in left and put it in put stuff to the left in left
+        left = merge_sort(arr[0: len(arr) // 2])
+    # sort to the right in the right in the right
+        right = merge_sort(arr[len(arr) // 2:])
 
-        #recrusion
-        merge_sort(lefthalf)
-        merge_sort(righthalf)
+    # Merge left and right
+        arr = merge(left, right)
 
-        i=0
-        j=0
-        k=0
-
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
-                arr[k]=lefthalf[i]
-                i=i+1
-            else:
-                arr[k]=righthalf[j]
-                j=j+1
-            k=k+1
-        while i < len(lefthalf):
-            arr[k]=lefthalf[i]
-            i=i+1
-            k=k+1
-        while i < len(righthalf):
-            arr[k]=lefthalf[j]
-            j=j+1
-            k=k+1
-# Merge them back together 
-# Compare the first values of each arra, add smaller of hte 2 to result 
     return arr
-print(merge_sort)
 
 
 # STRETCH: implement an in-place merge sort algorithm
